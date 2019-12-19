24b3622b9111a11054b4f5e167828dab7afc60c7
Aditya Chettri <reactb6@gmail.com>
2019-12-19 11:52:53 +0530

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
 
 const mockUserData = [{
     name: "Mark"
@@ -24,6 +26,29 @@ app.get('/users/:id', function (req, res) {
         user: req.params.id
     })
 })
+
+
+app.post('/login', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockusername = "billythe";
+    const mockpassword = "superkeys";
+
+    if (username == mockusername && password == mockpassword) {
+        res.json({
+            success: true,
+            message: "password valid",
+            token: "token goes here"
+        })
+    } else {
+        res.json({
+            success: false,
+            message: "password and username doesnot match"
+        })
+    }
+})
+
 
 app.listen(8000, function () {
     console.log("server is running");
